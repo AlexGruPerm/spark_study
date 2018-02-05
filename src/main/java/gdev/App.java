55a5272fbd326c4abadf25cec5ef225af5735f8b
AlexGruPerm <ugr@bk.ru>
2018-02-06 02:07:51 +0500

    Last commit call application in Windows 7, connect to Spark master and execute command codes inside.

--- src/main/java/gdev/App.java
+++ src/main/java/gdev/App.java
@@ -1,6 +1,22 @@
 package gdev;
 
+import static org.apache.parquet.schema.OriginalType.TIMESTAMP_MILLIS;
+import static org.apache.parquet.schema.OriginalType.UTF8;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT64;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Logger;
+import org.apache.parquet.Preconditions;
+import org.apache.parquet.example.data.Group;
+import org.apache.parquet.example.data.simple.SimpleGroupFactory;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.example.ExampleParquetWriter;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.Types;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -9,42 +25,74 @@ import org.apache.spark.sql.SQLContext;
 //import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class App {
 
 	final static Logger logger = Logger.getLogger(App.class);
 	
 	public static void main(String[] args) {
-		String warehouseLocation = "file:"+"/root/spark-warehouse";
 		logger.info("info message");
 		logger.debug("debug message");
-		logger.info("Sys user.dir : "+ System.getProperty("user.dir"));
 		
 	       SparkSession spark = SparkSession
 	    	    	.builder()
-	    		    .appName("Java Spark SQL xyz 333")
+	    		    .appName("Java Spark SQL") 
 	    		    .config("spark.master", "spark://192.168.1.8:7077")
-	    		    .config("spark.sql.warehouse.dir", warehouseLocation) //Path does not exist: file:/C:/spark_study/employees.json;
-	    		    //.config("spark.sql.warehouse.dir", "hdfs://192.168.1.8:50070/user/hive/warehouse/")
-	    		    //.enableHiveSupport()
-	    		    //.config(CATALOG_IMPLEMENTATION.key, "hive")
 	    		    .getOrCreate();
 	       
-/*
- SparkSession sparkSession = SparkSession
-            .builder().appName(JsonToHive.class.getName())
-            //.config("spark.sql.warehouse.dir", "hdfs://localhost:50070/user/hive/warehouse/")
-            .enableHiveSupport().master(master).getOrCreate();
-*/
+	       logger.debug("CONF:"+spark.conf().getAll().toString()); 
+		/*
+		 * 2018-02-06 02:03:27 4188 [main] DEBUG gdev.App -
+		 * CONF:Map(spark.driver.host -> 192.168.1.5, 
+		 * spark.driver.port ->
+		 * 59749, spark.app.name -> Java Spark SQL, 
+		 * spark.executor.id -> driver,
+		 * spark.master -> spark://192.168.1.8:7077, 
+		 * spark.app.id ->
+		 * app-20180205160323-0017)
+		 */
 	       
-	       SQLContext sqlCtx = spark.sqlContext();
-	       Dataset<Row> rowDataset = sqlCtx.read().json("employees.json");
-	       rowDataset.printSchema();
-	       rowDataset.createOrReplaceGlobalTempView("employeesData");
 	       
-	       //JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
-	       //logger.info(">>>>>>>>> jsc.getSparkHome="+jsc.getSparkHome().toString()); // jsc.getSparkHome=Optional[C:\spark-2.2.1-bin-hadoop2.7]
+	       Dataset<Row> usersDF = spark.read().load("/root/data/users.parquet"); 
+	       
+	        
+		/*
+		try{
+	       MessageType DS_FILE_SCHEMA = Types.buildMessage()
+		  		      .required(BINARY).as(UTF8).named("name")
+		  		      .named("dataset");
+	       //example: https://www.programcreek.com/java-api-examples/index.php?api=parquet.hadoop.ParquetWriter
+	        SimpleGroupFactory GROUP_FACTORY_DS = new SimpleGroupFactory(DS_FILE_SCHEMA);
+ 
+	  	    File fl_ds = new File("/root/data");
+	  	    logger.info("Recreate parquet file for app_loaded events: "+fl_ds.getAbsolutePath());
+	  	    if (fl_ds.exists()==true) {
+		      Preconditions.checkArgument(fl_ds.delete(), "Could not remove parquet file "+fl_ds.getAbsolutePath());
+	  	    }
+		    Path pth_ds = new Path(fl_ds.toString());
+	       
+		    ParquetWriter<Group> writer = ExampleParquetWriter.builder(pth_ds)
+			        .withType(DS_FILE_SCHEMA)
+			        .build();
+		    
+		    for (int j = 0;  j < 100; ++j) {
+	        	    	Group group1 = GROUP_FACTORY_DS.newGroup(); 
+	        		    group1.add("name",      "name_" + j);
+	        		    writer.write(group1);
+	        		    //.append("timestamp", System.currentTimeMillis());
+	        		    writer.write(group1);
+	        }
+		    writer.close();
 
-	       spark.close();
-	}
+		}
+		     catch (IOException e) {
+			  logger.error(e.fillInStackTrace());
+			 }
+		*/
+		        spark.close();
+		}
+
 
 }
