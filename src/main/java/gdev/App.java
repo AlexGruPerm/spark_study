a2e85ab0bda9ce6e2f5d2fe9dd59c65e121a67ba
AlexGruPerm <ugr@bk.ru>
2018-02-08 11:45:01 +0500

    Read json generated from Java and save,load into parquet

--- src/main/java/gdev/App.java
+++ src/main/java/gdev/App.java
@@ -12,8 +12,14 @@ import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT64;
 
 //import org.apache.hadoop.fs.Path;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
  
+import org.apache.parquet.Preconditions;
 /*
 import org.apache.parquet.Preconditions;
 import org.apache.parquet.example.data.Group;
@@ -27,10 +33,13 @@ import org.apache.spark.SparkConf;
 //import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 //import org.apache.spark.sql.SQLContext;
 //import org.apache.spark.sql.Dataset;
 //import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+//import org.apache.hadoop.fs.Path;
+//import org.junit.rules.TemporaryFolder;
 
 //import com.fasterxml.jackson.core.JsonToken;
 //import com.fasterxml.jackson.databind.JsonNode;
@@ -44,23 +53,40 @@ public class App {
 		logger.debug("debug message");
 		
 		//String jsonPath = "/root/data/people.json";
-		String jsonPath = "C:\\spark_data\\people.json";
+		String jsonPath = "C:\\spark_data\\people3.json";
 		
 		//String parquetPath = "/root/data/names.parquet";
-		//String parquetPath = "C:\\spark_data\\names.parquet";
+		String parquetPath = "C:\\spark_data\\names.parquet";
 		
 		//String sparkMaster = "spark://192.168.1.8:7077"; 
 		
 		//String sparkMaster = "spark://192.168.1.5:7077";
-		String sparkMaster = "local[*]";
+		//"spark://169.254.16.5:7077"
+		
+		
+		String sparkMaster = "spark://169.254.16.5:7077";
+		//String sparkMaster = "local[*]";
 		  
 		//=======================================================
+		
+		//+File.separator+
 		
 		SparkConf conf = new SparkConf();
 		conf.set("spark.master",sparkMaster);
 		conf.set("spark.sql.crossJoin.enabled", "true");
-		//conf.setJars(new String[]{"C:\\spark_data\\gdev.jar"});
 
+		/*
+		File fl_prq = new File(parquetPath);	   
+	  	    if (fl_prq.exists()==true) {
+	  	      logger.info("Recreate parquet file for app_loaded events: "+fl_prq.getAbsolutePath());
+		      Preconditions.checkArgument(fl_prq.delete(), "Could not remove parquet file "+fl_prq.getAbsolutePath());
+	  	    } else {
+	  	      logger.info("SUCCESS File does not exists : "+fl_prq.getAbsolutePath());
+	  	    }
+	  	    */
+	  	//Path pth_file_prq = new Path(fl_prq.toString());
+	  	  
+	  	    
 
 	       SparkSession spark = SparkSession
 	    	    	.builder()
@@ -68,34 +94,45 @@ public class App {
 	    		    .config(conf)
 	    		    .getOrCreate();
 
-		
-		
-/* GOOD
-	       SparkSession spark = SparkSession
-	    	       	.builder()
-	       	        .appName("Java Spark SQL") 
-	       	        .config("spark.master", "local[*]")
-		            .config("spark.sql.crossJoin.enabled", "true")
-		            .getOrCreate();
-*/
-
 	       logger.debug("CONF:"+spark.conf().getAll().toString()); 
-	
+
+	    FileWriter f0;
+		try {
+			f0 = new FileWriter("C:\\spark_data\\people3.json");
+		       for(int i=0; i < 1000000; i++)
+		       {
+				f0.write("{\"name\":\""+RandomStringUtils.randomAlphanumeric(10).toUpperCase()+"\"}");
+		       }
+		       f0.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      // String newLine = System.getProperty("line.separator");
+
+	       
 	       Dataset<Row> df = spark.read().option("header","false").json(jsonPath);
-
 	       df.printSchema();
-
 	       df.show();
-	
+	       df.select("name").write().mode(SaveMode.Overwrite).parquet(parquetPath);
+        
+
+	       Dataset<Row> ds = spark.read().load(parquetPath);
 	       
-	       /*
-	       String prq_full_filename = parquetPath;
-	       
-	       df.select("name").write().format("parquet").save(prq_full_filename);
-	       
-	     	Dataset<Row> ds = spark.read().load(prq_full_filename);
-    	    ds.show();
-    	    ds.printSchema();
+    	   ds.show();
+    	   ds.printSchema();
+
+    	   Dataset<Row> sql_ds = spark.read().load(parquetPath);
+    	   sql_ds.createOrReplaceTempView("vusr");
+
+   		   Dataset<Row> sql_ds_res = spark.sql("SELECT name, count(*) as grp_by_key FROM vusr group by name order by 1 desc");
+   		   sql_ds_res.show(); 
+
+
+   	
+   		   /*
+   		   Dataset<Row> sql_ds_cnt = spark.sql("SELECT count(*) as cnt_rows_parquet FROM vusr");
+   		   sql_ds_cnt.show();   
 	       */
 	      
 	       /*
